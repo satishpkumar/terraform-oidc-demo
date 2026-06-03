b82a04d442d320e99b9a3ded116d2c85e5aef873
satish kumar <peddinti.satishkumar@gmail.com>
2026-06-03 14:59:51 +0530

    second and ami correction

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web_server" {
   ami           = ami-0685bcc683dadb6b9
-  instance_type = "t2.nano"
+
+  instance_type = "t3.micro"
 
   tags = {
     Name        = "Terraform-EC2-Example"
